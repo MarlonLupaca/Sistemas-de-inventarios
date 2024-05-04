5104ddea571192abc66e43df740cdb0b8a171dee
MarlonLupaca <marlondiegomendoza@gmail.com>
2024-05-03 23:14:13 -0500

    plantilla de login y de dashboard, puede ser modificada y es un estilo minimalista DiseñoV03

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,16 @@
 const desplegar = document.getElementById("desplegar");
 const barralateral = document.querySelector(".barralateral");
 const nombresBarra = document.querySelectorAll(".text");
-const icono = document.querySelector(".des");
+//const icono = document.querySelector(".des");
+const main = document.querySelector(".contenido--principal")
+const header = document.querySelector(".contenedor--cabecera")
 
 desplegar.addEventListener("click", () => {
+    //alert("hola");
     barralateral.classList.toggle("barralateral-mini");
-    icono.classList.toggle("des2");
+    main.classList.toggle("contenido--flexible")
+    header.classList.toggle("contenedor--cabecera--movimiento")
+    //icono.classList.toggle("des2");
     nombresBarra.forEach(elemento => {
         elemento.classList.toggle("menuDes");
     });
